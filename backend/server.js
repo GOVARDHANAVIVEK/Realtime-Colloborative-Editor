a07cf51fc6392551d3d11b8fce30cd83d4620a1a
Vivek Govardhana <govardhanavivek32@gmail.com>
2025-02-26 21:22:30 +0530

    latest commit

--- backend/server.js
+++ backend/server.js
@@ -27,7 +27,7 @@ connectMongooseDB();
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({ extended: false }));
 app.use(cors({
-    origin: ["http://localhost:5173","https://realtime-colloborative-editor.vercel.app/"],
+    origin: ["http://localhost:5173","https://realtime-colloborative-editor-1.onrender.com"],
     credentials: true
 }));
 app.use(
